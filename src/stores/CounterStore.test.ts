2e843d239cc55d65d1a9e57dc6f2c02ff334fb71
Minhye Kang <alsgp9898@naver.com>
2023-03-12 22:14:05 +0900

    test: 테스트 코드 중복 제거

--- src/stores/CounterStore.test.ts
+++ src/stores/CounterStore.test.ts
@@ -5,18 +5,21 @@ const context = describe;
 describe('CounterStore', () => {
   let counterStore: CounterStore;
 
-  const handleChange = jest.fn();
-
   beforeEach(() => {
     counterStore = new CounterStore();
   });
+
+  it('addListener', () => {
+    const handleChange = jest.fn();
+
+    counterStore.addListener(handleChange);
+    counterStore.increase();
+
+    expect(handleChange).toBeCalled();
+  });
   context('when click increase button', () => {
     it('count is 1', () => {
-      counterStore.addListener(handleChange);
-
       counterStore.increase();
-
-      expect(handleChange).toBeCalled();
 
       expect(counterStore.getSnapshot()).toEqual({ count: 1 });
     });
@@ -25,22 +28,14 @@ describe('CounterStore', () => {
     it('count is 10', () => {
       const step = 10;
 
-      counterStore.addListener(handleChange);
-
       counterStore.increase(step);
-
-      expect(handleChange).toBeCalled();
 
       expect(counterStore.getSnapshot()).toEqual({ count: 10 });
     });
   });
   context('when click decrease button', () => {
     it('count is -1', () => {
-      counterStore.addListener(handleChange);
-
       counterStore.decrease();
-
-      expect(handleChange).toBeCalled();
 
       expect(counterStore.getSnapshot()).toEqual({ count: -1 });
     });
@@ -49,11 +44,7 @@ describe('CounterStore', () => {
     it('count is -10', () => {
       const step = 10;
 
-      counterStore.addListener(handleChange);
-
       counterStore.decrease(step);
-
-      expect(handleChange).toBeCalled();
 
       expect(counterStore.getSnapshot()).toEqual({ count: -10 });
     });
