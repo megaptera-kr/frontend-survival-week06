d89a7cd9a2a973884215e813d9e84993ff93099b
박진우 <otterpark94@gmail.com>
2023-05-22 01:55:09 +0900

    Fix: 불필요한 hook 제거

--- src/hooks/useForceUpdate.ts
+++
@@ -1,6 +0,0 @@
-import { useCallback, useState } from 'react';
-
-export default function useForceUpdate<T>() {
-  const [, setState] = useState<T>({} as T);
-  return useCallback(() => setState({} as T), []);
-}
