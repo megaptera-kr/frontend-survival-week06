bf6196c151ed0d485f90f789bb83f2ed22b23982
OD <kg-te@naver.com>
2023-05-21 22:27:43 +0900

    ✨ App 에 컴포넌트 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
+import Cart from './components/Cart';
+import FilterableRestaurantsList from './components/FilterableRestaurantsList';
 import useFetchRestaurants from './hooks/useFetchRestaurants';
 
 export default function App() {
-
   const restaurants = useFetchRestaurants();
 
   return (
     <div>
       <h1>메가테라 푸드코트 키오스크</h1>
-      <ul>{restaurants && restaurants.map((item, index) => {
-        return (<li key={item.id}>{item.name}</li>);
-      })
-      }</ul>
+      <Cart />
+      <FilterableRestaurantsList restaurants={restaurants} />
+      <h2>영수증</h2>
     </div>
   );
 }
