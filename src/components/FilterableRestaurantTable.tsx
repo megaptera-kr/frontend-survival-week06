1886ec462a9bd0c2bfd1defa756f791598450a89
shcha <>
2023-10-29 23:53:02 +0900

    검색부분 반영

---
+++ src/components/FilterableRestaurantTable.tsx
@@ -0,0 +1,28 @@
+import { fromPairs } from "lodash";
+import React, { useState } from "react";
+import useFetchRestaurants from "../hooks/useFetchRestaurants";
+import filterRestaurants from "../utils/filterRestaurants";
+import selectCategories from "../utils/selectCategories";
+import SearchBar from "./SearchBar";
+
+export default function FilterableRestaurantTable(){
+    const restaurants = useFetchRestaurants();
+    const [filterText, setFilterText] = useState<string>('');
+    const [filterCategory, setFilterCategory] = useState<string>('전체');
+    const categories = selectCategories(restaurants);
+    const filteredRestaurants = filterRestaurants(restaurants, {
+        filterText, filterCategory,
+    });
+
+    return (
+        <div>
+          <SearchBar
+            categories={categories}
+            filterText={filterText}
+            setFilterText={setFilterText}
+            setFilterCategory={setFilterCategory}
+          />
+          {/* <RestaurantTable restaurants={filteredRestaurants} /> */}
+        </div>
+      );
+}
