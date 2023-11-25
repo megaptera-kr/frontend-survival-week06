bd86ede572d00354236d1ca8991017b736eb15fe
shinbom <tlsqha@gmail.com>
2023-11-25 10:20:20 +0900

    feat : Restaruant API Test 추가

--- src/components/Restauraunts/Restauraunts.test.ts
+++ src/components/Restauraunts/Restauraunts.test.ts
@@ -1,20 +1,37 @@
+import restaurants from "../../features/restaurants";'../../features/restaurants'
+
 // @ts-ignore
 const context= describe
-const getRestaurants = () => {
 
+const getRestaurants = async () => {
+    try {
+        const response = await fetch('http://localhost:3000/restaurants')
+        if(response.ok){
+            return await response.json()
+        }
+    } catch (err) {
+        throw Error('레스토랑 목록 조회에 실패하였습니다.')
+    }
 }
 
+const getCategories = () => {
+    return restaurants.reduce((acc, cur) => acc.category === cur.category ? [...cur.category] : cur , [])
+}
 describe('레스토랑 리스트를 가져올 API가 필요하다.',  () => {
     context('레스토랑 리스트를 가져온다.', () => {
         it('레스토랑 리스트가 조회된다.',   async () => {
-            const response = await fetch('http://localhost:3000/restaurants')
-            console.log(response)
+            const { restaurants } = await getRestaurants()
+            expect(restaurants.length > 0).toBeTruthy()
         })
     })
 });
 
-// describe('test', () => {
-//     it('test' , () => {
-//         fetch('localhost')
-//     })
-// })
+describe('매장의 분류가 필요하다.', () => {
+    context('레스토랑 리스트에서 분류만 추출한다.' , () => {
+        it('분류로 구성된 배열이 추출된다.', () => {
+            const categories = getCategories()
+            console.log(categories)
+            expect(categories.length === 3).toBeTruthy()
+        })
+    })
+})
