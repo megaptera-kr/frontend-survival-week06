677a2b57268888b3b1ac06fb2d125e014ccbc4f1
swd <tladnjsen@gmail.com>
2024-03-07 13:46:27 +0900

    [Frontend] Add fixture data categories

---
+++ fixtures/categories.ts
@@ -0,0 +1,14 @@
+import Category from '../src/types/CategoryType';
+
+const categories: Category[] = [
+  {
+    id: 1,
+    name: '한식',
+  },
+  {
+    id: 2,
+    name: '일식',
+  },
+];
+
+export default categories;
--- fixtures/index.ts
+++ fixtures/index.ts
@@ -1,9 +1,11 @@
 import foods from './foods';
 import receipt from './receipt';
 import restaurants from './restaurants';
+import categories from './categories';
 
 export default {
   foods,
   receipt,
   restaurants,
+  categories,
 };
