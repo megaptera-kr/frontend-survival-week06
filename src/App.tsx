e2c4dc98bd6c62f7dbdd006c42c5786d67acb8d4
swd <tladnjsen@gmail.com>
2024-03-07 18:18:12 +0900

    [Frontend] Add component - ReceiptPrinter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import KioskTitle from './components/KioskTitle';
 import Cart from './components/Cart';
 import FilterableRestaurantTable from './components/FilterableRestaurantTable';
+import ReceiptPrinter from './components/ReceiptPrinter';
 
 export default function App() {
   return (
@@ -8,39 +9,7 @@ export default function App() {
       <KioskTitle title='푸드코트 키오스크' />
       <Cart />
       <FilterableRestaurantTable />
-      <div
-        style={{
-          width: '50%',
-          border: '1px solid black',
-          textAlign: 'center',
-        }}
-      >
-        <p>[영수증 나오는 곳]</p>
-        <h2>영수증</h2>
-        <div>
-          <h3>주문번호</h3>
-          <p>123123213</p>
-        </div>
-        <div>
-          <h3>주문목록</h3>
-          <ul
-            style={{
-              padding: 0,
-              listStyle: 'none',
-            }}
-          >
-            <li
-              style={{
-                display: 'flex',
-                paddingBlock: '.5rem',
-              }}
-            >
-              <span style={{ margin: '0 auto' }}>짜장면(8,000원)</span>
-            </li>
-          </ul>
-        </div>
-        <p>총 가격: 8,000원</p>
-      </div>
+      <ReceiptPrinter />
     </div>
   );
 }
---
+++ src/components/ReceiptPrinter.tsx
@@ -0,0 +1,37 @@
+export default function ReceiptPrinter() {
+  return (
+    <div
+      style={{
+        width: '50%',
+        border: '1px solid black',
+        textAlign: 'center',
+      }}
+    >
+      <p>[영수증 나오는 곳]</p>
+      <h2>영수증</h2>
+      <div>
+        <h3>주문번호</h3>
+        <p>123123213</p>
+      </div>
+      <div>
+        <h3>주문목록</h3>
+        <ul
+          style={{
+            padding: 0,
+            listStyle: 'none',
+          }}
+        >
+          <li
+            style={{
+              display: 'flex',
+              paddingBlock: '.5rem',
+            }}
+          >
+            <span style={{ margin: '0 auto' }}>짜장면(8,000원)</span>
+          </li>
+        </ul>
+      </div>
+      <p>총 가격: 8,000원</p>
+    </div>
+  );
+}
