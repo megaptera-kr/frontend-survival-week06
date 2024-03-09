c16b7a1e336f9cc15030901a5f529e25634c2abe
swd <tladnjsen@gmail.com>
2024-03-09 23:38:52 +0900

    [Frontend] Modify ReceiptPrinter component

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,18 @@ import Cart from './components/Cart';
 import FilterableRestaurantTable from './components/FilterableRestaurantTable';
 import ReceiptPrinter from './components/ReceiptPrinter';
 
+import useReceiptStore from './hooks/useReceiptStore';
+
 export default function App() {
+  const [snapshot] = useReceiptStore();
+  const { item } = snapshot;
+
   return (
     <div>
       <KioskTitle title='푸드코트 키오스크' />
       <Cart />
       <FilterableRestaurantTable />
-      <ReceiptPrinter />
+      <ReceiptPrinter receipt={item} />
     </div>
   );
 }
