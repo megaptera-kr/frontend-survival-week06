735aeb5cc7ac718d29dec3909e2b2f6dcc27574a
swd <tladnjsen@gmail.com>
2024-03-09 23:42:49 +0900

    [Frontend] Show receipt just only 5 seconds

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useInterval } from 'usehooks-ts';
+
 import KioskTitle from './components/KioskTitle';
 import Cart from './components/Cart';
 import FilterableRestaurantTable from './components/FilterableRestaurantTable';
@@ -6,15 +8,24 @@ import ReceiptPrinter from './components/ReceiptPrinter';
 import useReceiptStore from './hooks/useReceiptStore';
 
 export default function App() {
-  const [snapshot] = useReceiptStore();
-  const { item } = snapshot;
+  const [snapshot, receiptStore] = useReceiptStore();
+  const { item: receipt } = snapshot;
+
+  useInterval(
+    () => {
+      if (receipt.id) {
+        receiptStore.clearItems();
+      }
+    },
+    receipt.id ? 5000 : null,
+  );
 
   return (
     <div>
       <KioskTitle title='푸드코트 키오스크' />
       <Cart />
       <FilterableRestaurantTable />
-      <ReceiptPrinter receipt={item} />
+      <ReceiptPrinter receipt={receipt} />
     </div>
   );
 }
