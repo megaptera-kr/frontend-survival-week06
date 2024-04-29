84cb36a52cb943514f88ef8d8270c6b0dec23e14
WATA-Yiju <yiju.dev@watanow.com>
2024-04-29 13:44:09 +0900

    fix:경로 수정

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -5,7 +5,7 @@ import fixture from '../../fixtures';
 const BASE_URL = 'http://localhost:3000';
 
 const handlers = [
-  rest.get(`${BASE_URL}/products`, (req, res, ctx) => {
+  rest.get(`${BASE_URL}/restaurants`, (req, res, ctx) => {
     const { restaurants } = fixture;
     return res(
       ctx.status(200),
