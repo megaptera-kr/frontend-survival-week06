46a081fc20d969517201528b9e288af1a60e42c7
WATA-Yiju <yiju.dev@watanow.com>
2024-04-29 16:52:39 +0900

    add:ordersType 추가 및 receiptState 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { useBoolean } from 'usehooks-ts';
 import { FilterableRestaurants, Orders, Receipt } from './components';
 
 // TODO: 메가테라 푸트코트 키오스크를 완성해주세요.
 export default function App() {
+  const {
+    value: receiptState,
+    setTrue: showReceipt,
+    setFalse: hideReceipt,
+  } = useBoolean(false);
   return (
     <div>
       <h1>메가테라 푸드코트 키오스크</h1>
--- src/utils/getAllCategories/getAllCategories.test.ts
+++ src/utils/getAllCategories/getAllCategories.test.ts
@@ -7,7 +7,13 @@ describe('getAllCategories func', () => {
   const { restaurants } = fixture;
   context('restaurants가 빈 배열이라면', () => {
     it('"전체"를 포함한 배열만 반환한다.', () => {
-      expect(getAllCategories([]));
+      expect(getAllCategories([])).toEqual(['전체']);
+    });
+  });
+
+  context('한식, 중식을 포함한 식당이 있으면', () => {
+    it('전체, 중식, 한식을 포함한 배열을 반환한다.', () => {
+      expect(getAllCategories(restaurants)).toEqual(['전체', '중식', '한식']);
     });
   });
 });
